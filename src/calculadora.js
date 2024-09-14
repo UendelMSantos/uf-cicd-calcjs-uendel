5f1283b32afb68491be8343016eebd09734bc673
UendelMSantos <uendelmartins1501@gmail.com>
2024-09-14 14:36:49 +0000

    updated calculadora of divide

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,8 +6,16 @@ function subtract(a, b){
     return a - b;
 }
 
+function divide(a, b) {
+    if (b === 0) {
+    throw new Error("Divisão por zero não é permitida.");
+    }
+    return a / b;
+}
+
 module.exports = {
     add,
-    subtract
+    subtract,
+    divide,
 };
 
